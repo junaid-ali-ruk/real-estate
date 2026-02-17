4b4a0f5280b76c74b8a6524cf48f20af5bae812b
junaid-ali-ruk <junaid2025.ali@gmail.com>
2026-02-17 03:09:27 -0800

    fix: add events.mapbox.com to CSP connect-src directive

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,7 @@ const cspHeader = `
   style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
   img-src 'self' blob: data: https://cdn.sanity.io https://img.clerk.com https://*.mapbox.com;
   font-src 'self' https://fonts.gstatic.com;
-  connect-src 'self' https://api.mapbox.com https://*.sanity.io https://*.clerk.accounts.dev https://clerk.nestwell.com;
+  connect-src 'self' https://api.mapbox.com https://events.mapbox.com https://*.mapbox.com https://*.sanity.io https://*.clerk.accounts.dev https://clerk.nestwell.com;
   frame-src 'self' https://*.clerk.accounts.dev https://*.clerk.com;
   worker-src 'self' blob:;
 `;
